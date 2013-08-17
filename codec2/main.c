d9e402503c5e84e69224339bee1ccf9c358b0ac4
Keith Gould <keithmgould@gmail.com>
2013-08-17 08:51:08 -0400

    cleaning up and adding comments

--- codec2/main.c
+++ codec2/main.c
@@ -1,39 +1,69 @@
-#include "codec2.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <math.h>
+#include "codec2.h"
 #include "portaudio.h"
-/*
-** Note that many of the older ISA sound cards on PCs do NOT support
-** full duplex audio (simultaneous record and playback).
-** And some only support full duplex at lower sample rates.
-*/
-#define SAMPLE_RATE         (8000)
-#define PA_SAMPLE_TYPE      paInt16
-#define FRAMES_PER_BUFFER   (320)
 
+
+/*----------------------------------------------------------------
+ * Since we are compressing and working with voice, we can go as
+ * low as possible.  6kHz might work but for now lets stick with
+ * 8kHz.
+ */
+#define SAMPLE_RATE         (8000)
+
+/*----------------------------------------------------------------
+ * The FRAMES_PER_BUFFER and CODEC2_MODE constansts are correlated.
+ * See codec2.c in the codec2 repo to learn more.  In short, do not
+ * change one without learning how to change the other.
+*/
+#define FRAMES_PER_BUFFER   (320)
+#define CODEC2_MODE         (CODEC2_MODE_1200)
+
+/*----------------------------------------------------------------
+ * The sample type and SAMPLE typedef are correlated.  If we were to
+ * use paFloat32 for example, the SAMPLE type would be float.
+ */
+#define PA_SAMPLE_TYPE      paInt16
 typedef short SAMPLE;
 
+
+/*----------------------------------------------------------------
+ * callbackData contains any information needed to persist between
+ * callbacks.  Right now this is just information, not because there
+ * is data needed between callbacks, but so that we don't have to
+ * initialize codec2 or its arrays repeatedly.
+ */
 typedef struct
 {
   void          *codec2;
-  int           nsam; // number of samples
-  int           nbit; // number of bits
-  int           nbyte;// number of bytes
-  short         *buf;
+  int           nsam;       // number of samples
+  int           nbit;       // number of bits
   unsigned char *bits;
 }
-paTestData;
+callbackData;
 
-static int fuzzCallback( const void *inputBuffer, void *outputBuffer,
+
+/*----------------------------------------------------------------
+ * localToRemoteCallback is called everytime the inputBuffer is full.
+ *
+ * local audio comes directly in from the user.
+ * remote audio comes in over the telco's line (the phone line)
+ *
+ * localToRemoteCallback's job is to take local audio and:
+ * 1) compress.  Using codec2
+ * 2) encrypt.  Using AES256
+ * 3) modulate.  Using IncDec (see "Hermes: Data Transmission over Unknown Voice Channels")
+ */
+static int localToRemoteCallback( const void *inputBuffer, void *outputBuffer,
                          unsigned long framesPerBuffer,
                          const PaStreamCallbackTimeInfo* timeInfo,
                          PaStreamCallbackFlags statusFlags,
                          void *userData )
 {
-    paTestData *data = (paTestData*)userData;
+    callbackData *data = (callbackData*)userData;
     SAMPLE *out = (SAMPLE*)outputBuffer;
     const SAMPLE *in = (const SAMPLE*)inputBuffer;
     int i;
@@ -77,32 +107,20 @@ int main(void)
     PaStreamParameters inputParameters, outputParameters;
     PaStream *stream;
     PaError err;
-    paTestData data;
+    callbackData data;
 
     err = Pa_Initialize();
     if( err != paNoError ) goto error;
 
-
     /* create a pointer to the codec states */
-    data.codec2 = codec2_create(CODEC2_MODE_1200);
+    data.codec2 = codec2_create(CODEC2_MODE);
 
-    /* calculate the number of samples per frame */
-    data.nsam = codec2_samples_per_frame(data.codec2);
-    printf("number of samples per frame: %d\n", data.nsam);
-
-    /* calculate the number of bits per frame */
+    /* determine the number of bits per frame */
     data.nbit = codec2_bits_per_frame(data.codec2);
-    printf("number of bits: %d\n", data.nbit);
 
-    /* calculate the number of bytes */
-    data.nbyte = (data.nbit + 7) / 8;
-    printf("number of bytes: %d\n", data.nbyte);
-
-    data.buf = malloc ( data.nsam * sizeof(short) );
+    /* allocate and clean space needed to store the compressed audio */
     data.bits = malloc (data.nbit * sizeof(char) );
-
     int i;
-    for (i = 0; i < data.nsam; i++) { data.buf[i] = 0; }
     for (i = 0; i < data.nbit; i++) { data.bits[i] = 0; }
 
     inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
@@ -132,7 +150,7 @@ int main(void)
               SAMPLE_RATE,
               FRAMES_PER_BUFFER,
               0, /* paClipOff, */  /* we won't output out of range samples so don't bother clipping them */
-              fuzzCallback,
+              localToRemoteCallback,
               &data );
     if( err != paNoError ) goto error;
 
@@ -146,14 +164,12 @@ int main(void)
 
     printf("Finished.\n");
     Pa_Terminate();
-    free(data.buf);
     free(data.bits);
     codec2_destroy(data.codec2);
     return 0;
 
 error:
     Pa_Terminate();
-    free(data.buf);
     free(data.bits);
     codec2_destroy(data.codec2);
     fprintf( stderr, "An error occured while using the portaudio stream\n" );
