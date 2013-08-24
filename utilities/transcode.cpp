39ed91d125310e5d4004de47743f158dbdb526a7
Keith Gould <keithmgould@gmail.com>
2013-08-24 16:57:23 -0400

    add comments to the transcoder

--- utilities/transcode.cpp
+++ utilities/transcode.cpp
@@ -2,6 +2,19 @@
 #include <bitset>
 using namespace std;
 
+/* Transcoder
+ *
+ * Trancode's job is to take bytes of data, and for each bit of each byte,
+ * transcode the bit.  The transcode rule is:
+ * if 1, output 10
+ * if 0, output 01
+ *
+ * Example:
+ * Input byte 0xD3.
+ * This would be 11010011 in binary.
+ * The transcoded bitstream would be: 1010011001011010.
+ *
+ */
 class Transcode {
   public:
   static void Perform(vector<unsigned char> &compressedBytes, vector<bool> &transcodedBits)
