1f582ee8d8184b7d44472cdcbcd000b682910e18
Keith Gould <keithmgould@gmail.com>
2013-09-05 07:38:38 -0400

    tests green again

--- utilities/test/transcode_test.cpp
+++ utilities/test/transcode_test.cpp
@@ -1,21 +1,24 @@
 #include <UnitTest++.h>
 #include "../transcode.cpp"
 
+#include <iostream>
+
 SUITE (transcode)
 {
-  TEST(Double)
+  TEST(perform)
   {
     // random byte input
-    // 00000000, 11111111, 01011101
-    unsigned char byteInput[3] = { 0x00, 0xFF, 0x5D};
+    // 0x00: 00000000
+    // 0xFF: 11111111
+    // 0x5D: 01011101
+    unsigned char byteInput[3] = { 0x00, 0xFF, 0x5D };
     vector<bool> transcodedBits;
-    Transcode::Double(byteInput, transcodedBits, 3);
-    // bool correctBits[48] =   {F,T,F,T,F,T,F,T,F,T,F,T,F,T,F,T,
-                              // T,F,T,F,T,F,T,F,T,F,T,F,T,F,T,F,
-                              // F,T,T,F,F,T,T,F,T,F,T,F,F,T,T,F};
-    // for(int i=0; i < 48; i++)
-    // {
-      // CHECK_EQUAL(correctBits[i], transcodedBits[i]);
-    // }
+    Transcode::Perform(byteInput, transcodedBits, 3);
+    bool correctBitsArr[54] =   {T,T,T,F,F,F,                       // preamble
+                                 F,T,F,T,F,T,F,T,F,T,F,T,F,T,F,T,   // 0x00
+                                 T,F,T,F,T,F,T,F,T,F,T,F,T,F,T,F,   // 0xFF
+                                 F,T,T,F,F,T,T,F,T,F,T,F,F,T,T,F};  // 0x5D
+    vector<bool> correctBits (correctBitsArr, correctBitsArr + sizeof(correctBitsArr) / sizeof(bool) );
+    CHECK(correctBits == transcodedBits);
   }
 }
