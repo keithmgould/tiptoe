1b053f80a50c7b3b5ad48a9f205241f2701db5bb
Keith Gould <keithmgould@gmail.com>
2013-09-08 15:02:49 -0400

    maxBits works on reverse_transcode

--- utilities/test/reverse_transcode_test.cpp
+++ utilities/test/reverse_transcode_test.cpp
@@ -3,7 +3,7 @@
 
 SUITE(reverse_transcode)
 {
-  TEST (perform_with_less_than_48_data_bits) {
+  TEST (perform_with_less_than_max_bits) {
     bool extractedBitsArr[10] = { T,F,F,F,T,F,T,F,T,T };
     vector<bool> extractedBits (extractedBitsArr, extractedBitsArr + sizeof(extractedBitsArr) / sizeof(bool) );
 
@@ -12,12 +12,23 @@ SUITE(reverse_transcode)
 
     vector<bool> dataBits;
 
-    ReverseTranscode rt(extractedBits);
+    ReverseTranscode rt(extractedBits,10);
     rt.perform(dataBits);
     CHECK(correctDataBits == dataBits);
   }
 
-  TEST (perform_with_more_than_48_data_bits) {
+  TEST (perform_with_more_than_max_bits) {
 
+    bool extractedBitsArr[10] = { T,F,F,F,T,F,T,F,T,T };
+    vector<bool> extractedBits (extractedBitsArr, extractedBitsArr + sizeof(extractedBitsArr) / sizeof(bool) );
+
+    bool correctDataBitsArr[5] = { T,F,F,F,F };
+    vector<bool> correctDataBits (correctDataBitsArr, correctDataBitsArr + sizeof(correctDataBitsArr) / sizeof(bool) );
+
+    vector<bool> dataBits;
+
+    ReverseTranscode rt(extractedBits,5);
+    rt.perform(dataBits);
+    CHECK(correctDataBits == dataBits);
   }
 }
