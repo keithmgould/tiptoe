a045e9042966079d33ddacf41b6510f212350673
Keith Gould <keithmgould@gmail.com>
2013-09-15 10:47:35 -0400

    adds in bit to char

--- utilities/convert.cpp
+++ utilities/convert.cpp
@@ -22,4 +22,37 @@ class Convert {
       }
     }
   }
+
+  struct octet {
+    union {
+      char byteVal;
+      struct {
+        unsigned h : 1;
+        unsigned g : 1;
+        unsigned f : 1;
+        unsigned e : 1;
+        unsigned d : 1;
+        unsigned c : 1;
+        unsigned b : 1;
+        unsigned a : 1;
+      };
+    };
+  };
+
+  static void BitsToUnsignedChar(vector<bool> &inputBits, unsigned char outputBytes[], int byteCount)
+  {
+    octet o;
+    for(int i=0; i<byteCount; i++ )
+    {
+      o.a = inputBits[8*i+0];
+      o.b = inputBits[8*i+1];
+      o.c = inputBits[8*i+2];
+      o.d = inputBits[8*i+3];
+      o.e = inputBits[8*i+4];
+      o.f = inputBits[8*i+5];
+      o.g = inputBits[8*i+6];
+      o.h = inputBits[8*i+7];
+      outputBytes[i] = o.byteVal;
+    }
+  }
 };
--- utilities/test/convert_test.cpp
+++ utilities/test/convert_test.cpp
@@ -10,4 +10,14 @@ SUITE (convert) {
     vector<bool> correctBits (correctBitsArr, correctBitsArr + sizeof(correctBitsArr) / sizeof(bool) );
     CHECK(correctBits == dataBits);
   }
+
+  TEST(bits_to_unsigned_char)
+  {
+    bool inputBitsArr[24] = {F,T,F,F,T,F,T,T, F,T,F,F,T,T,F,T,  F,T,F,F,F,T,T,T};
+    vector<bool> inputBits (inputBitsArr, inputBitsArr + sizeof(inputBitsArr) / sizeof(bool) );
+    unsigned char correctBytes[3] = { 0x4B, 0x4D, 0x47 };
+    unsigned char outputBytes[3];
+    Convert::BitsToUnsignedChar(inputBits, outputBytes, 3);
+    CHECK_ARRAY_EQUAL(correctBytes, outputBytes,3);
+  }
 }
