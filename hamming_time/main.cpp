3ff151c5f6dcd3990b9096ec82f5cff8b8bb7b30
Keith Gould <keithmgould@gmail.com>
2013-10-12 17:20:16 -0400

    small file to prove out speed of 7 bit parity hamming encodig

---
+++ hamming_time/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+#include <sys/time.h>
+#include <vector>
+#include "../utilities/include/hamming.h"
+using namespace std;
+
+int main()
+{
+  const int parity_bit_size = 7;
+  int data_bit_size = pow(2,parity_bit_size) - parity_bit_size - 1;
+  vector<bool> input;
+  vector<bool> output;
+
+  for(int i=0;i<data_bit_size;i++)
+  {
+    input.push_back(1);
+  }
+  Hamming h(parity_bit_size);
+
+  timeval time;
+  gettimeofday(&time, NULL);
+  long millis = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+  cout <<  millis << endl;
+
+  h.encode(input, output);
+
+  gettimeofday(&time, NULL);
+  millis = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+  cout <<  millis << endl;
+  return 0;
+}
