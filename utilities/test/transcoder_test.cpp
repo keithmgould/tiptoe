6274a218d26c4ef250d42969bf3de9bb38e063c7
Keith Gould <keithmgould@gmail.com>
2013-10-17 07:46:10 -0400

    transcoder green in suite again

--- utilities/test/transcoder_test.cpp
+++ utilities/test/transcoder_test.cpp
@@ -1,21 +1,20 @@
 #include "gmock/gmock.h"
-#include "../constants.cpp"
-#include "../transcode.cpp"
+#include "../include/constants.h"
+#include "../src/transcoder.cpp"
 
-TEST (transcode, perform)
+TEST (transcoder, transcode)
 {
   bool rawBitsArr[24] = { 0,0,0,0,0,0,0,0,
                           1,1,1,1,1,1,1,1,
                           0,1,0,1,1,1,0,1 };
   vector<bool> rawBits (rawBitsArr, rawBitsArr + sizeof(rawBitsArr) / sizeof(bool) );
   vector<bool> transcodedBits;
-  Transcode::Perform(rawBits, transcodedBits);
-  bool correctBitsArr[45] =   {1,1,1,1,0,                     // preamble
-                               0,0,1,0,1,0,1,0,1,0,1,0,1,0,1, // data: 0x00
+  Transcoder::Transcode(rawBits, transcodedBits);
+  bool correctBitsArr[40] =   {0,0,1,0,1,0,1,0,1,0,1,0,1,0,1, // data: 0x00
                                1,1,0,1,0,1,0,1,0,1,0,1,0,1,0, // data: 0xFF
                                0,1,0,1,1,0,1,0,0,1};          // data: 0x5D
 
 
   vector<bool> correctBits (correctBitsArr, correctBitsArr + sizeof(correctBitsArr) / sizeof(bool) );
-  ASSERT_TRUE(correctBits == transcodedBits);
+  ASSERT_THAT(correctBits, testing::ContainerEq(transcodedBits));
 }
