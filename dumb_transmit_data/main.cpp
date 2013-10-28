b5525653411474da6537fcf2590512bca4c17701
Keith Gould <keithmgould@gmail.com>
2013-10-28 08:30:41 -0400

    adding dumb_transmit_data

---
+++ dumb_transmit_data/main.cpp
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <csignal>
+#include <fstream>
+#include <stdlib.h>
+#include "portaudio.h"
+#include "../utilities/include/constants.h"
+#include "../utilities/include/converter.h"
+#include "../utilities/include/dumb_transmitter.h"
+
+using namespace std;
+
+typedef short SAMPLE;
+
+/*----------------------------------------------------------------
+ * Callback Data.  Persists across buffers.  Used here to hold on
+ * to large objects that take time to instantiate.  The Transmitter
+ * stores the waveforms, for example.
+ */
+typedef struct
+{
+  // Points to the instance of codec2
+  // CODEC2                  *codec2;
+
+  // Points to the instance of the transmitter, which holds
+  // waveforms that are expensive to compute, so we do this
+  // only once.
+  Transmitter             *transmitter;
+
+  // Used to ensure both sides of the conversation are in
+  // sync.
+  int                     bufferCounter;
+}
+callbackData;
+
+static int transmitCallback( const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData )
+{
+  callbackData *data = (callbackData*)userData;
+  SAMPLE *out = (SAMPLE*)outputBuffer;
+
+  (void) timeInfo; /* Prevent unused variable warnings. */
+  (void) statusFlags;
+  (void) inputBuffer;
+
+  // 6 bytes (48 bits) will come from Codec2 so faux input: 6 random bytes
+  unsigned char faux[6] = { 0xD3, 0xA9, 0x5D, 0x2D, 0xBC, 0x94 }; //110100111010100101011101001011011011110010010100
+  // unsigned char faux[6] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, };   //101010101010101010101010101010101010101010101010
+  // convert and push unsigned chars onto dataBits
+  vector<bool> dataBits;
+  vector<bool> transcodedBits;
+  Converter::UnsignedCharToBits(faux, dataBits, 6);
+  data->transmitter->emitSound(out);
+  return paContinue;
+}
+
+/*******************************************************************/
+int main(int argc, char *argv[])
+{
+  int device_id;
+  if(argc != 2)
+  {
+    cout << "Usage: " <<argv[0] << " <device id (int)>" << endl;
+    cout << "try running port-audio's pa_devs binary" << endl;
+    cout << "or if you are on a Raspberry Pi, try $aplay -l" << endl;
+    exit(1);
+  }else{
+    device_id = atoi(argv[1]);
+  }
+  PaStreamParameters outputParameters;
+  PaStream *stream;
+  PaError err;
+  callbackData data;
+
+  Transmitter transmitter(FRAMES_PER_BUFFER);
+  data.transmitter = &transmitter;
+
+  data.bufferCounter = 0;
+
+  err = Pa_Initialize();
+  if( err != paNoError ) goto error;
+
+  outputParameters.device = device_id;// Pa_GetDefaultOutputDevice(); /* default output device */
+  if (outputParameters.device == paNoDevice) {
+    fprintf(stderr,"Error: No default output device.\n");
+    goto error;
+  }
+  outputParameters.channelCount = 2;       /* stereo output */
+  outputParameters.sampleFormat = paInt16;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+
+  err = Pa_OpenStream(
+      &stream,
+      NULL, /* no input */
+      &outputParameters,
+      SAMPLE_RATE,
+      FRAMES_PER_BUFFER,
+      paClipOff,
+      transmitCallback,
+      &data );
+  if( err != paNoError ) goto error;
+
+  err = Pa_StartStream( stream );
+  if( err != paNoError ) goto error;
+
+  cout << "Hit ENTER to stop program." << endl;
+  getchar();
+  err = Pa_CloseStream( stream );
+  if( err != paNoError ) goto error;
+
+  Pa_Terminate();
+  printf("Finished.\n");
+
+  return err;
+error:
+  Pa_Terminate();
+  fprintf( stderr, "An error occured while using the portaudio stream\n" );
+  fprintf( stderr, "Error number: %d\n", err );
+  return err;
+}
+
+
---
+++ utilities/src/dumb_transmitter.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include "../include/constants.h"
+#include "../include/transmitter.h"
+
+/*
+  The Transmitter's job is to produce audio that encodes
+  the data.
+ */
+
+/*
+ The Constructor is expensive, as it builds waveforms that
+ correspond to each of the five frequencies used by the
+ Tiptoe protocol.
+ */
+Transmitter::Transmitter (int framesPerBuffer)
+{
+  this->framesPerBuffer = framesPerBuffer;
+  this->tContainer.waveformSizes[PREAMBLE_LOW] = (double) SAMPLE_RATE / (double) PREAMBLE_LOW_FREQUENCY;
+  this->tContainer.waveformSizes[EDGE_LOW] = (double) SAMPLE_RATE / (double) EDGE_LOW_FREQUENCY;
+  this->tContainer.waveformSizes[MIDDLE_LOW] = (double) SAMPLE_RATE / (double) MIDDLE_LOW_FREQUENCY;
+  this->tContainer.waveformSizes[MIDDLE_HIGH] = (double) SAMPLE_RATE / (double) MIDDLE_HIGH_FREQUENCY;
+  this->tContainer.waveformSizes[EDGE_HIGH] = (double) SAMPLE_RATE / (double) EDGE_HIGH_FREQUENCY;
+  buildWaveforms();
+}
+
+/*
+ Resets the transmitter with the new data to send
+*/
+
+void Transmitter::setBits ( vector<bool> &transcodedBits )
+{
+
+  this->transcodedBits = transcodedBits;
+  this->bitIterator = this->transcodedBits.begin();
+}
+
+/*
+  Emit Sound
+  This function places the proper sinusoids in the output buffer
+
+  The function's single argument is a pointer to the Portaudio
+  ouput buffer.
+*/
+void Transmitter::emitSound( short *out )
+{
+  bool nextSinusoid = false;
+  int phase = 0;
+  int mode = MIDDLE_LOW;
+  float amp;
+  float fpb;
+  fpb = this->framesPerBuffer;
+  for(int i=0; i < fpb; i++ )
+  {
+    if(nextSinusoid)
+    {
+      mode = this->determineNextMode(mode, i);
+      nextSinusoid = false;
+      phase = 0;
+    }
+
+    // Here we play with amplitude, shaping it to look like spoken
+    // words in order to trick the telco codecs.
+    amp = 0.5;
+    if( i <= (fpb / 2))
+    {
+      amp += i/fpb;
+    }else{
+      amp += (fpb - i) / fpb;
+    }
+    if(this->amplitudeMode)
+    {
+      amp = amp * this->tContainer.waveforms[mode + 100].at(phase++);
+    }else{
+      amp = amp * this->tContainer.waveforms[mode].at(phase++);
+    }
+    // output the same thing on both channels
+    *out++ = amp;
+    *out++ = amp;
+    if(phase >= this->tContainer.waveformSizes[mode]) { nextSinusoid = true;}
+  }
+}
+
+/*
+   Build Wave Forms
+   Builds the waveforms used to generate sound.
+   This is done using the standard definition of sine, and then converted
+   into a type short via multiplication of 32767.
+
+   We also find the amplitude at 70%.  This is because every half second we
+   switch from one amplitude to another to defeat Voice Activity Detection (VAD).
+   See http://en.wikipedia.org/wiki/Voice_activity_detection
+*/
+
+void Transmitter::buildWaveforms()
+{
+  int i;
+
+  map<int,double>::iterator it = this->tContainer.waveformSizes.begin();
+  short sample;
+
+  // for each waveform size
+  for(;it != this->tContainer.waveformSizes.end(); it++)
+  {
+    cout << "wav " << it->first << ": ";
+    for( i=0; i< it->second; i++ )
+    {
+      // Full amplitude waveforms
+      sample = (short) (sin( ((double)i/it->second) * M_PI * 2.0) * 32767);
+      cout << sample << ", ";
+      this->tContainer.waveforms[it->first].push_back(sample);
+
+      // 70% amplitude waveforms.
+      sample = sample * SMALLER_AMPLITUDE;
+      this->tContainer.waveforms[it->first + 100].push_back(sample);
+    }
+    cout << endl;
+  }
+}
+
+/*
+  Determine Next Mode.
+
+  modulation is achieved using the IncDec algorithm found in the
+  Hermes paper.
+
+  Once all the data is modulated, the function returns zeros and ones until the output
+  buffer is full.
+
+  TODO: Refactor this huge function
+ */
+int Transmitter::determineNextMode(int mode, int onFrame)
+{
+  mode = (mode + 1) % 2;
+
+  // if we are in the second half of a second, add 100 to the mode
+  // to adjust the amplitude.
+  timeval time;
+  gettimeofday(&time, NULL);
+  long millis = time.tv_usec / 1000;
+  millis = millis % 1000;
+  amplitudeMode = (millis < 250) || (millis > 500 && millis < 750);
+  return mode;
+};
